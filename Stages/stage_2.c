8ffbfbee8c4d98c157ffa0e0d0ac4d6c853620c0
n8han11 <157491264+n8han11@users.noreply.github.com>
2024-01-25 19:11:12 +0000

    Stage 2 code
    Adding the work I've done for Stage 2 so far (rough draft, but most external shell commands should work).

---
+++ Stages/stage_2.c
@@ -0,0 +1,27 @@
+// stage_2.c
+
+#include "stage_2.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void execute_external_command(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) { // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("shell");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("shell");
+    } else { // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+}
