2d8406e6c50d6a9e247ad640bad20c7a39c6a3a3
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2024-02-19 17:51:01 +0000

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
-int main()
-{
+int main(){
 	//Find the user home directory from the environment (3)
+	
 	char* home_dir = GetHomeDirectory();
 
 	//Save the current path (3)
@@ -20,8 +20,7 @@ int main()
 	//Load aliases (8)
 	
 	//Do while shell has not terminated
-	while(1)
-	{
+	while(1){
 		//Display prompt (1)
 		display_prompt();
 
@@ -34,6 +33,10 @@ int main()
 		}
 		if (args[0] != NULL && strcmp("exit", args[0]) == 0) break;
 
+		// for testing purposes
+		for (int i = 0; *(args + i) != NULL; i++) {
+			printf("%s\n", *(args + i));
+		}
 		//While the command is a history invocation or alias then replace it 
 		//with the appropriate command from history or the aliased command 
 		//respectively (5 & 7)
@@ -60,16 +63,34 @@ int main()
 				printf("New path changed to %s\n", newPath);
 			}
 		}
-		//Else execute command as an external process (2)
-		else
+		//(Lex): for the cd part.
+		if(strcmp("cd", args[0]) == 0)
 		{
-        	execute_external_command(args);
+			printf("successfully entered the shid\n");	
+			if(*(args + 1) == NULL)
+			{
+				printf(GetPathEnv());
+				ChangeDirectory(GetPathEnv());
+			}
+			else
+				ChangeDirectory(*(args + 1));
 		}
 		
+		//(Nat): this bit is just a placeholder to make sure the external_command goes through; replace this specific execute_external_command with "built-in invoke appropriate function" later
+		if (1) {
+			execute_external_command(args);
+		}
+		
+		//Else execute command as an external process (2)
+		else {
+        		execute_external_command(args);
+   	}
 		free(*args); //frees the buffer (stage_1.c)
 		free(args); //frees the result (stage_1.c)
 	}
+	// End while (okay yes this comment is pointless)
 	
+	//From here down *could* be a separate exit function, depends on your logic
 	//Save history (6)
 	
 	//Save aliases (8)
